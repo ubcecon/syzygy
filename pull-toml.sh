70d46891259534080891b9b299b183a8429d96a5
Arnav Sood <soodarnav01@gmail.com>
2018-10-15 20:31:42 -0700

    revise pull-toml script'

--- pull-toml.sh
+++ pull-toml.sh
@@ -1,3 +1,3 @@
 # Will need to make sure wget is installed, if you're using macOS. 
-wget https://raw.githubusercontent.com/QuantEcon/lecture-source-jl/master/notebooks/Project.toml -O Project.toml
-wget https://raw.githubusercontent.com/QuantEcon/lecture-source-jl/master/notebooks/Manifest.toml -O Manifest.toml
+wget https://raw.githubusercontent.com/QuantEcon/QuantEconLecturePackages/master/Manifest.toml -O Manifest.toml
+wget https://raw.githubusercontent.com/QuantEcon/QuantEconLecturePackages/master/Project.toml -O Project.toml
