4c97407234491a7979898989df786ca16c8e58c7
Arnav Sood <soodarnav01@gmail.com>
2018-10-17 12:14:34 -0700

    iterate

--- init.sh
+++ init.sh
@@ -1,5 +1,3 @@
 # Only run if the .julia doesn't exist
-if [ ! -d /home/jupyter/.julia/ ]; then
-    mkdir -p /home/jupyter/.julia # Create the directory structure if it doesn't exist. 
-    cp -r /home/jovyan/.julia/environments /home/jupyter/.julia/ # Copy in our environments
-fi
+mkdir -p /home/jupyter/.julia # Create the directory structure if it doesn't exist. 
+cp -r /home/jovyan/.julia/environments /home/jupyter/.julia/ # Copy in our environments
